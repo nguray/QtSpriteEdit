2c1dd13da00b47929121534bce17990e5ee5ea74
Raymond NGUYEN THANH <raymond.nguyenthanh@gmail.com>
2024-07-08 20:45:18 +0200

    Update NewSpriteDlg

--- newspritedlg.h
+++ newspritedlg.h
@@ -2,26 +2,26 @@
 #define NEWSPRITEDLG_H
 
 #include <QDialog>
-#include <QPushButton>
 #include <QLineEdit>
+#include <QPushButton>
+#include <QSpinBox>
 
-class NewSpriteDlg : public QDialog
-{
-    Q_OBJECT
+
+class NewSpriteDlg : public QDialog {
+  Q_OBJECT
 public:
-    NewSpriteDlg(QWidget *parent = nullptr);
-    QPushButton *m_okBtn;
-    QPushButton *m_cancelBtn;
-    QLineEdit   *m_widthEdit;
-    QLineEdit   *m_heightEdit;
+  NewSpriteDlg(QWidget *parent = nullptr);
+  QPushButton   *m_okBtn;
+  QPushButton   *m_cancelBtn;
+  QSpinBox      *m_widthEdit;
+  QSpinBox      *m_heightEdit;
 
-    int         m_width;
-    int         m_height;
+  int m_width;
+  int m_height;
 
 private slots:
-    void    okBtnClick();
-    void    cancelBtnClick();
-
+  void okBtnClick();
+  void cancelBtnClick();
 };
 
 #endif // NEWSPRITEDLG_H
