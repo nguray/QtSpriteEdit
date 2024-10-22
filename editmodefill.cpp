97132460c3f07bca680228196e5a9574999611a8
Raymond NGUYEN THANH <raymond.nguyenthanh@gmail.com>
2024-10-22 15:29:54 +0200

    Remove typo

--- editmodefill.cpp
+++ editmodefill.cpp
@@ -1,225 +1,210 @@
 #include "editmodefill.h"
 #include "editarea.h"
-#include <stack>
 #include <QMouseEvent>
+#include <stack>
 
-EditModeFill::EditModeFill()
-{
 
-}
+EditModeFill::EditModeFill() {}
 
-void EditModeFill::FloodFill( int fillX, int fillY, QColor fillColor)
-{
-    int                 width, height;
-    QColor              targetColor,curColor;
-    QPoint              pt;
-    std::stack<QPoint>  stk;
-    int                 x,y;
-    int                 startX,startY;
-    int                 xEndLine,xStartLine;
-    bool                fStartNord,
-                        fNord,
-                        fStartSud,
-                        fSud;
-    //-----------------------------------------------------------------------
+void EditModeFill::FloodFill(int fillX, int fillY, QColor fillColor) {
+  int width, height;
+  QColor targetColor, curColor;
+  QPoint pt;
+  std::stack<QPoint> stk;
+  int x, y;
+  int startX, startY;
+  int xEndLine, xStartLine;
+  bool fStartNord, fNord, fStartSud, fSud;
+  //-----------------------------------------------------------------------
 
-    if (!m_image) return;
+  if (!m_image)
+    return;
 
-    targetColor = m_image->pixelColor( fillX, fillY);
+  targetColor = m_image->pixelColor(fillX, fillY);
 
-    if ((!targetColor.isValid())||(targetColor==fillColor)) return;
+  if ((!targetColor.isValid()) || (targetColor == fillColor))
+    return;
 
-    width = m_image->width();
-    height = m_image->height();
+  width = m_image->width();
+  height = m_image->height();
 
-    stk.push(QPoint(fillX,fillY));
+  stk.push(QPoint(fillX, fillY));
 
-    while (!stk.empty()){
+  while (!stk.empty()) {
 
-        startX = stk.top().x();
-        startY = stk.top().y();
-        stk.pop();
+    startX = stk.top().x();
+    startY = stk.top().y();
+    stk.pop();
+
+    //-- Vérifier au Nord
+    fStartNord = false;
+
+    if (startY > 0) {
+      curColor = m_image->pixelColor(startX, startY - 1);
+      if (curColor.isValid()) {
+        if (curColor == targetColor) {
+          stk.push(QPoint(startX, startY - 1));
+          fStartNord = true;
+        }
+      }
+    }
+
+    //-- Vérifier au sud
+    fStartSud = false;
+    if ((startY + 1) < height) {
+      curColor = m_image->pixelColor(startX, startY + 1);
+      if (curColor.isValid()) {
+        if (curColor == targetColor) {
+          stk.push(QPoint(startX, startY + 1));
+          fStartSud = true;
+        }
+      }
+    }
+
+    y = startY;
+    //-- Aller vers l'est
+    xEndLine = startX;
+    fNord = fStartNord;
+    fSud = fStartSud;
+    if (xEndLine < (width - 1)) {
+
+      x = xEndLine + 1;
+      while (x < width) {
+        curColor = m_image->pixelColor(x, y);
+        if (!curColor.isValid())
+          break;
+        if (curColor != targetColor) {
+          break;
+        }
 
         //-- Vérifier au Nord
-        fStartNord = false;
-
-        if (startY>0){
-            curColor = m_image->pixelColor(startX,startY-1);
-            if (curColor.isValid()){
-                if (curColor==targetColor){
-                    stk.push(QPoint(startX,startY-1));
-                    fStartNord = true;
-                }
+        if ((y - 1) > 0) {
+          curColor = m_image->pixelColor(x, y - 1);
+          if ((curColor.isValid()) && (curColor == targetColor)) {
+            if (!fNord) {
+              stk.push(QPoint(x, y - 1));
+              fNord = true;
             }
+          } else {
+            fNord = false;
+          }
+        } else {
+          fNord = false;
         }
-        return true;
 
         //-- Vérifier au sud
-        fStartSud = false;
-        if ((startY+1)<height){
-            curColor = m_image->pixelColor(startX,startY+1);
-            if (curColor.isValid()){
-                if (curColor==targetColor){
-                    stk.push(QPoint(startX,startY+1));
-                    fStartSud = true;
-                }
+        if (y < (height - 1)) {
+          curColor = m_image->pixelColor(x, y + 1);
+          if (curColor.isValid() && (curColor == targetColor)) {
+            if (!fSud) {
+              stk.push(QPoint(x, y + 1));
+              fSud = true;
             }
+          } else {
+            fSud = false;
+          }
+        } else {
+          fSud = false;
         }
+        xEndLine = x;
+        x++;
+      }
 
-        y = startY;
-        //-- Aller vers l'est
-        xEndLine = startX;
-        fNord = fStartNord;
-        fSud = fStartSud;
-        if (xEndLine<(width-1)){
-
-            x = xEndLine + 1;
-            while(x<width){
-                curColor = m_image->pixelColor(x,y);
-                if (!curColor.isValid()) break;
-                if (curColor!=targetColor){
-                    break;
-                }
-
-                //-- Vérifier au Nord
-                if ((y-1)>0){
-                    curColor = m_image->pixelColor(x,y-1);
-                    if ((curColor.isValid())&&(curColor==targetColor)){
-                        if (!fNord){
-                            stk.push(QPoint(x,y-1));
-                            fNord = true;
-                        }
-                    }else{
-                        fNord = false;
-                    }
-                }else{
-                    fNord = false;
-                }
-
-                //-- Vérifier au sud
-                if (y<(height-
-                             return true;1)){
-                    curColor = m_image->pixelColor(x,y+1);
-                    if (curColor.isValid()&&(curColor==targetColor)){
-                        if (!fSud){
-                            stk.push(QPoint(x,y+1));
-                            fSud = true;
-                        }
-                    }else{
-                        fSud = false;
-                    }
-                }else{
-                    fSud = false;
-                }
-                xEndLine = x;
-                x++;
-            }
-
-        }else{
-            xEndLine = width-1;
-        }
-
-        //-- Aller vers l'ouest
-        xStartLine = startX;
-        fNord = fStartNord;
-        fSud = fStartSud;
-        if (xStartLine>0){
-            x = xStartLine - 1;
-            while(x>=0){
-
-                curColor = m_image->pixelColor(x,y);
-                if (!curColor.isValid()) break;
-                if (curColor!=targetColor) break;
-
-                //-- Vérifier au Nord
-                if ((y-1)>0){
-                    curColor = m_image->pixelColor(x,y-1);
-                    if (curColor.isValid()&&(curColor==targetColor)){
-                        if (!fNord){
-                            stk.push( QPoint(x,y-1));
-                            fNord = true;
-                        }
-                    }else{
-                        fNord = false;
-                    }
-                }else{
-                    fNord = false;
-                }
-
-                //-- Vérifier au sud
-                if (y<(height-1)){
-                    curColor = m_image->pixelColor(x,y+1);
-                    if (curColor.isValid()&&(curColor==targetColor)){
-                        if (!fSud){
-                            stk.push(QPoint(x,y+1));
-                            fSud = true;
-                        }
-                    }else{
-                        fSud = false;
-                    }
-                }else{
-                    fSud = false;
-                }
-                xStartLine = x;
-                x--;
-            }
-
-        }else{
-            xStartLine = 0;
-        }
-        //-- Tracer la line
-        for(x=xStartLine;x<=xEndLine;x++){
-            m_image->setPixelColor( x, y, fillColor);
-
-        }
-
+    } else {
+      xEndLine = width - 1;
     }
 
+    //-- Aller vers l'ouest
+    xStartLine = startX;
+    fNord = fStartNord;
+    fSud = fStartSud;
+    if (xStartLine > 0) {
+      x = xStartLine - 1;
+      while (x >= 0) {
 
-}
+        curColor = m_image->pixelColor(x, y);
+        if (!curColor.isValid())
+          break;
+        if (curColor != targetColor)
+          break;
 
-void EditModeFill::paintEvent(QWidget *w,QPainter &painter)
-{
-    // if (m_image){
-    //      painter.drawImage(QPoint(200,200),*m_imageBackup);
-    // }
-}
-
-bool EditModeFill::mousePressEvent(QWidget *w,QMouseEvent *event)
-{
-    if (event->button()==Qt::LeftButton) {
-        int pixelX,pixelY;
-        QTransform trans = m_transform_scale * m_transform_translate;
-        QPoint pt = trans.map(event->pos());
-        if (m_image){
-            if (mouseToPixel( pt.x(), pt.y(), pixelX, pixelY)){
-                saveState();
-                FloodFill( pixelX, pixelY, m_foreGroundColor);
-                emit ((editarea *) w)->editSpriteChanged();
-                w->update();
+        //-- Vérifier au Nord
+        if ((y - 1) > 0) {
+          curColor = m_image->pixelColor(x, y - 1);
+          if (curColor.isValid() && (curColor == targetColor)) {
+            if (!fNord) {
+              stk.push(QPoint(x, y - 1));
+              fNord = true;
             }
+          } else {
+            fNord = false;
+          }
+        } else {
+          fNord = false;
         }
+
+        //-- Vérifier au sud
+        if (y < (height - 1)) {
+          curColor = m_image->pixelColor(x, y + 1);
+          if (curColor.isValid() && (curColor == targetColor)) {
+            if (!fSud) {
+              stk.push(QPoint(x, y + 1));
+              fSud = true;
+            }
+          } else {
+            fSud = false;
+          }
+        } else {
+          fSud = false;
+        }
+        xStartLine = x;
+        x--;
+      }
+
+    } else {
+      xStartLine = 0;
     }
-    return false;
+    //-- Tracer la line
+    for (x = xStartLine; x <= xEndLine; x++) {
+      m_image->setPixelColor(x, y, fillColor);
+    }
+  }
 }
 
-bool EditModeFill::mouseReleaseEvent(QWidget *w,QMouseEvent *event)
-{
-    return false;
+void EditModeFill::paintEvent(QWidget *w, QPainter &painter) {
+  // if (m_image){
+  //      painter.drawImage(QPoint(200,200),*m_imageBackup);
+  // }
 }
 
-bool EditModeFill::mouseDoubleClickEvent(QWidget *w,QMouseEvent *event)
-{
-    return false;
+bool EditModeFill::mousePressEvent(QWidget *w, QMouseEvent *event) {
+  if (event->button() == Qt::LeftButton) {
+    int pixelX, pixelY;
+    QTransform trans = m_transform_scale * m_transform_translate;
+    QPoint pt = trans.map(event->pos());
+    if (m_image) {
+      if (mouseToPixel(pt.x(), pt.y(), pixelX, pixelY)) {
+        saveState();
+        FloodFill(pixelX, pixelY, m_foreGroundColor);
+        emit((editarea *)w)->editSpriteChanged();
+        w->update();
+      }
+    }
+  }
+  return false;
 }
 
-bool EditModeFill::mouseMoveEvent(QWidget *w,QMouseEvent *event)
-{
-
-    return false;
+bool EditModeFill::mouseReleaseEvent(QWidget *w, QMouseEvent *event) {
+  return false;
 }
 
-void EditModeFill::resizeEvent(QWidget *w,QResizeEvent *event)
-{
-
+bool EditModeFill::mouseDoubleClickEvent(QWidget *w, QMouseEvent *event) {
+  return false;
 }
+
+bool EditModeFill::mouseMoveEvent(QWidget *w, QMouseEvent *event) {
+
+  return false;
+}
+
+void EditModeFill::resizeEvent(QWidget *w, QResizeEvent *event) {}
